38b56f4b5cbd292a2224978825a587391b7ed58c
yashverma <yash.verma1200@gmail.com>
2020-03-13 17:36:05 +0530

    Completed

--- app/src/main/java/com/tenten/checkerror/MainActivity.java
+++ app/src/main/java/com/tenten/checkerror/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.tenten.checkinternetconnectionanderrors.ErrorDialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -17,7 +18,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ErrorDialog errorDialog;
-    
+private boolean check=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +26,118 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        errorDialogInit();
+        //errorDialogInit();
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                errorDialog.show();
+//             check=    new ErrorDialog.Builder(MainActivity.this)
+//                .setBackgroundGradient(getResources().getColor(R.color.colorAccent),getResources().getColor(R.color.colorPrimaryDark))
+//                        .setCancelButtonStrokeColor(getResources().getColor(R.color.greenYellow))
+//                        .setCancelButtonTextColor(getResources().getColor(R.color.dark_red))
+//                        .setReryButtonTextColor(getResources().getColor(R.color.dark_goldenrod))
+//                        .setReryButtonColor(getResources().getColor(R.color.light_pink))
+//                        .setErrorOccuredHeading("yash Verma")
+//                        .setErrorOccuredSubText("hehaheha")
+//                        .setNoInternetHeading("nhi ara tower")
+//                        .setNoInternetSubText("Kya ukhad lega")
+//                        .setRetryButtonClickListner(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                Toast.makeText(MainActivity.this, "Retry fuck it", Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//                        .setCancelButtonClickListner(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                Toast.makeText(MainActivity.this, "Cancel it", Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//                        .buildCheckInternetOnly();
+//                if (check){
+//                    Toast.makeText(getApplicationContext(), "Found", Toast.LENGTH_SHORT).show();
+//                }else {
+//                    Toast.makeText(getApplicationContext(), "Not Found", Toast.LENGTH_SHORT).show();
+//                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //TODO FULL BUILD
+                    new ErrorDialog.Builder(MainActivity.this)
+                        .setBackgroundGradient(getResources().getColor(R.color.colorAccent),getResources().getColor(R.color.colorPrimaryDark))
+                        .setCancelButtonStrokeColor(getResources().getColor(R.color.greenYellow))
+                        .setCancelButtonTextColor(getResources().getColor(R.color.dark_red))
+                        .setReryButtonTextColor(getResources().getColor(R.color.dark_goldenrod))
+                        .setReryButtonColor(getResources().getColor(R.color.light_pink))
+                        .setErrorOccuredHeading("yash Verma")
+                        .setErrorOccuredSubText("hehaheha")
+                        .setNoInternetHeading("nhi ara tower")
+                        .setNoInternetSubText("Kya ukhad lega")
+                        .setRetryButtonClickListner(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "Retry fuck it", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setCancelButtonClickListner(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "Cancel it", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .build();
+
+
             }
         });
-    }
-
-    private void setErrorDialog() {
-        if (isFinishing()) {
-            if (!errorDialog.isShowing())
-                errorDialog.show();
-            Toast.makeText(MainActivity.this, "Clicked1", Toast.LENGTH_SHORT).show();
-        }
-    }
 
 
-    private void errorDialogInit() {
-        errorDialog = new ErrorDialog(this);
-        errorDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        errorDialog.setCancelable(false);
-        errorDialog.setCanceledOnTouchOutside(false);
-        errorDialog.buttonColor();
     }
+
+//    private void errorDialogInit() {
+//        errorDialog = new ErrorDialog(this);
+//        errorDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        errorDialog.setCancelable(false);
+//        errorDialog.setCanceledOnTouchOutside(false);
+//       // errorDialog.setBackground(1,2);
+//
+//    }
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- checkinternetconnectionanderrors/src/main/java/com/tenten/checkinternetconnectionanderrors/ErrorDialog.java
+++ checkinternetconnectionanderrors/src/main/java/com/tenten/checkinternetconnectionanderrors/ErrorDialog.java
@@ -5,125 +5,309 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
+
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.HashMap;
 
 public class ErrorDialog extends Dialog {
     public Activity activity;
-    private String lblHeading = "ERROR OCCURRED", lblSubtext = "It seem's that some error have been occurred", lottie = "error1.json";
+    private final View.OnClickListener retryListner, cancellistner;
+    private String lblHeading = null, lblSubtext = null, lottie = "error1.json";
+
+    private String nonetLblHeading = null, nonetlblSubtext = null, nonetlottie = "nonet.json";
     private TextView heading, subtext;
-    private Button retryButton, canclButton;
+    private MaterialButton retryButton, canclButton;
     private LottieAnimationView lottieAnimationView;
     private RelativeLayout relativeLayout;
+    private int gradientStart, gradientEnd;
+    private boolean check = false;
+    private int cancelStrokeColor, retryBtnColor, retrybtnTextColor, canclBtnTextColor;
 
 
-    public ErrorDialog(Activity activity) {
+    private ErrorDialog(Activity activity, int gradientStart, int gradientEnd, int cancelStrokeColor, int canclBtnTextColor, int retryBtnColor, int retryBtnTextColor, String lblHeading, String lblSubtext, String nonetLblHeading, String nonetlblSubtext
+            , View.OnClickListener retryListner, View.OnClickListener cancellistner) {
         super(activity);
         this.activity = activity;
-        this.lblHeading = lblHeading;
-        this.lblSubtext = lblSubtext;
-        this.lottie = lottie;
+        this.gradientStart = gradientStart == 0 ? Color.parseColor("#FFAB00") : gradientStart;
+        this.gradientEnd = gradientEnd == 0 ? Color.parseColor("#FFD600") : gradientEnd;
+        this.cancelStrokeColor = cancelStrokeColor == 0 ? Color.parseColor("#ffffff") : cancelStrokeColor;
+        this.canclBtnTextColor = canclBtnTextColor == 0 ? Color.parseColor("#ffffff") : canclBtnTextColor;
 
+        this.retryBtnColor = retryBtnColor == 0 ? Color.parseColor("#000000") : retryBtnColor;
+        this.retrybtnTextColor = retryBtnTextColor == 0 ? Color.parseColor("#ffffff") : retryBtnTextColor;
+        this.lblHeading = lblHeading == null ? "NO INTERNET" : lblHeading;
+        this.lblSubtext = lblSubtext == null ? "It seem's that you are not connected" : lblSubtext;
+
+        this.nonetLblHeading = nonetLblHeading == null ? "ERROR OCCURRED" : nonetLblHeading;
+        this.nonetlblSubtext = nonetlblSubtext == null ? "It seem's that some error have been occurred" : nonetlblSubtext;
+        this.retryListner = retryListner;
+        this.cancellistner = cancellistner;
     }
 
-    public void retryButtonSetColor(ColorStateList colorStateList) {
-        retryButton = findViewById(R.id.retry);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            retryButton.setBackgroundTintList(colorStateList);
-        }
 
-    }
-
-    public void cancelButtonSetColor(ColorStateList colorStateList) {
-        canclButton = findViewById(R.id.Cancel);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            canclButton.setBackgroundTintList(colorStateList);
-        }
-
-    }
-
-    public ErrorDialog(Activity activity, String lblHeading, String lblSubtext, String lottie) {
+    private ErrorDialog(Activity activity, int gradientStart, int gradientEnd, int cancelStrokeColor, int canclBtnTextColor, int retryBtnColor, int retryBtnTextColor, String lblHeading, String lblSubtext, String nonetLblHeading, String nonetlblSubtext
+            , View.OnClickListener retryListner, View.OnClickListener cancellistner, boolean check) {
         super(activity);
         this.activity = activity;
-        this.lblHeading = lblHeading;
-        this.lblSubtext = lblSubtext;
-        this.lottie = lottie;
+        this.gradientStart = gradientStart == 0 ? Color.parseColor("#FFAB00") : gradientStart;
+        this.gradientEnd = gradientEnd == 0 ? Color.parseColor("#FFD600") : gradientEnd;
+        this.cancelStrokeColor = cancelStrokeColor == 0 ? Color.parseColor("#ffffff") : cancelStrokeColor;
+        this.canclBtnTextColor = canclBtnTextColor == 0 ? Color.parseColor("#ffffff") : canclBtnTextColor;
 
+        this.retryBtnColor = retryBtnColor == 0 ? Color.parseColor("#000000") : retryBtnColor;
+        this.retrybtnTextColor = retryBtnTextColor == 0 ? Color.parseColor("#ffffff") : retryBtnTextColor;
+        this.lblHeading = lblHeading == null ? "NO INTERNET" : lblHeading;
+        this.lblSubtext = lblSubtext == null ? "It seem's that you are not connected" : lblSubtext;
+
+        this.nonetLblHeading = nonetLblHeading == null ? "ERROR OCCURRED" : nonetLblHeading;
+        this.nonetlblSubtext = nonetlblSubtext == null ? "It seem's that some error have been occurred" : nonetlblSubtext;
+        this.retryListner = retryListner;
+        this.cancellistner = cancellistner;
+        this.check = check;
     }
 
-    public void setBackground(int start,int end){
-        relativeLayout=findViewById(R.id.relativeLayout);
-        int[] colors = {Color.parseColor("#008000"),Color.parseColor("#ADFF2F")};
-
-        //create a new gradient color
-        GradientDrawable gd = new GradientDrawable(
-                GradientDrawable.Orientation.TOP_BOTTOM, colors);
-
-        gd.setCornerRadius(0f);
-
-
-        relativeLayout.setBackground(gd);
-
-    }
-
-
-    private boolean isNetworkConnected() {
-        ConnectivityManager cm = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_layout);
-        relativeLayout=findViewById(R.id.relativeLayout);
-        heading = findViewById(R.id.dialogHeading);
-        subtext = findViewById(R.id.dialogSubtext);
-        lottieAnimationView = findViewById(R.id.lottieDialog);
-        retryButton = findViewById(R.id.retry);
-        canclButton = findViewById(R.id.Cancel);
+        initViews();
+        setBackground();
+        setButtonBackgroundColor();
+
+        checkConnectAndSetText();
+        setCancelButtonTextColor();
+        setRetryButtonBackgroundColor();
+        setRetryButtonTextColor();
+
+
+        if (!onlyInternet()) {
+            heading.setText(nonetLblHeading);
+            subtext.setText(nonetlblSubtext);
+            lottieAnimationView.setAnimation(nonetlottie);
+        }
+
+
         canclButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dismiss();
-                activity.finish();
+                if (cancellistner == null) {
+                    dismiss();
+                    activity.finish();
+                } else {
+                    cancellistner.onClick(view);
+                }
+
             }
         });
-
-        if (isNetworkConnected()) {
-            heading.setText(lblHeading);
-            subtext.setText(lblSubtext);
-            lottieAnimationView.setAnimation(lottie);
-        } else {
-            heading.setText("NO INTERNET");
-            subtext.setText("It seem's that you are not connected");
-            lottieAnimationView.setAnimation("nonet.json");
-        }
 
 
         retryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dismiss();
 
-                activity.finish();
-                activity.overridePendingTransition(0, 0);
-                activity.startActivity(activity.getIntent());
-                activity.overridePendingTransition(0, 0);
+                if (retryListner == null) {
+                    dismiss();
+
+                    activity.finish();
+                    activity.overridePendingTransition(0, 0);
+                    activity.startActivity(activity.getIntent());
+                    activity.overridePendingTransition(0, 0);
+                } else {
+                    retryListner.onClick(view);
+                }
 
             }
         });
 
 
     }
+
+    public void setBackground() {
+        relativeLayout = findViewById(R.id.relativeLayout);
+        int[] colors = {gradientStart, gradientEnd};
+        GradientDrawable gd = new GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM, colors);
+        gd.setCornerRadius(20);
+        relativeLayout.setBackground(gd);
+
+    }
+
+    public void setButtonBackgroundColor() {
+        canclButton.setStrokeColor(ColorStateList.valueOf(cancelStrokeColor));
+
+    }
+
+    public void setCancelButtonTextColor() {
+        canclButton.setTextColor(canclBtnTextColor);
+
+    }
+
+
+    public void setRetryButtonBackgroundColor() {
+        retryButton.setBackgroundColor(retryBtnColor);
+
+    }
+
+    public void setRetryButtonTextColor() {
+        retryButton.setTextColor(retrybtnTextColor);
+
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+
+    private void checkConnectAndSetText() {
+        if (isNetworkConnected()) {
+            heading.setText(lblHeading);
+            subtext.setText(lblSubtext);
+            lottieAnimationView.setAnimation(lottie);
+        } else {
+            heading.setText(nonetLblHeading);
+            subtext.setText(nonetlblSubtext);
+            lottieAnimationView.setAnimation(nonetlottie);
+        }
+    }
+
+    private boolean onlyInternet() {
+        if (isNetworkConnected()) {
+            return true;
+        } else {
+           return false;
+        }
+    }
+
+    private void initViews() {
+        relativeLayout = findViewById(R.id.relativeLayout);
+        heading = findViewById(R.id.dialogHeading);
+        subtext = findViewById(R.id.dialogSubtext);
+        lottieAnimationView = findViewById(R.id.lottieDialog);
+        retryButton = findViewById(R.id.retry);
+        canclButton = findViewById(R.id.Cancel);
+    }
+
+    public static class Builder {
+        private int startgradient, endgradient, strokecolor, canclBtnTextColor, retryBtnColor, retrybtnTextColor;
+        private Activity activity;
+        private String lblHeading, lblSubtext, nonetLblHeading, nonetlblSubtext;
+        private View.OnClickListener onClickListener, cancellistner;
+
+        public Builder(Activity activity) {
+            this.activity = activity;
+
+        }
+
+        /**
+         * @param startgradient
+         * @param endgradient
+         * @return
+         */
+        public Builder setBackgroundGradient(int startgradient, int endgradient) {
+            this.startgradient = startgradient;
+            this.endgradient = endgradient;
+            return this;
+        }
+
+        public Builder setCancelButtonStrokeColor(int strokecolor) {
+            this.strokecolor = strokecolor;
+            return this;
+        }
+
+        public Builder setCancelButtonTextColor(int canclBtnTextColor) {
+            this.canclBtnTextColor = canclBtnTextColor;
+            return this;
+        }
+
+
+        public Builder setReryButtonTextColor(int retrybtnTextColor) {
+            this.retrybtnTextColor = retrybtnTextColor;
+            return this;
+        }
+
+        public Builder setReryButtonColor(int retryBtnColor) {
+            this.retryBtnColor = retryBtnColor;
+            return this;
+        }
+
+
+        public Builder setErrorOccuredHeading(String lblHeading) {
+            this.lblHeading = lblHeading;
+            return this;
+        }
+
+        public Builder setErrorOccuredSubText(String lblSubtext) {
+            this.lblSubtext = lblSubtext;
+            return this;
+        }
+
+
+        public Builder setNoInternetHeading(String nonetLblHeading) {
+            this.nonetLblHeading = nonetLblHeading;
+            return this;
+        }
+
+        public Builder setNoInternetSubText(String nonetlblSubtext) {
+            this.nonetlblSubtext = nonetlblSubtext;
+            return this;
+        }
+
+        public Builder setRetryButtonClickListner(View.OnClickListener onClickListener) {
+            this.onClickListener = onClickListener;
+            return this;
+        }
+
+
+        public Builder setCancelButtonClickListner(View.OnClickListener cancellistner) {
+            this.cancellistner = cancellistner;
+            return this;
+        }
+
+
+        public ErrorDialog build() {
+            ErrorDialog errorDialog = new ErrorDialog(activity, startgradient, endgradient, strokecolor, canclBtnTextColor, retryBtnColor, retrybtnTextColor, lblHeading, lblSubtext, nonetLblHeading, nonetlblSubtext,
+                    onClickListener, cancellistner);
+            errorDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            errorDialog.setCanceledOnTouchOutside(true);
+            errorDialog.setCancelable(false);
+            errorDialog.show();
+            return errorDialog;
+        }
+
+        public boolean buildCheckInternetOnly() {
+
+            ErrorDialog errorDialog = new ErrorDialog(activity, startgradient, endgradient, strokecolor, canclBtnTextColor, retryBtnColor, retrybtnTextColor, lblHeading, lblSubtext, nonetLblHeading, nonetlblSubtext,
+                    onClickListener, cancellistner, true);
+            errorDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            errorDialog.setCanceledOnTouchOutside(true);
+            errorDialog.setCancelable(false);
+            if (!errorDialog.onlyInternet()) {
+                errorDialog.show();
+                return false;
+            } else {
+                return true;
+            }
+
+
+        }
+
+    }
 }
+
